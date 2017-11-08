3652302acf98341eb010af82ca3e1577f239cb4e
sami <salman.rahman@se14.qmul.ac.uk>
2017-11-08 21:01:53 +0000

    fixed carriage return issue for non-OSX system

--- src/main/java/TokenizerMapper.java
+++ src/main/java/TokenizerMapper.java
@@ -12,16 +12,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-	private final IntWritable one = new IntWritable(1);
+    private final IntWritable one = new IntWritable(1);
     private Text data = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
         StringTokenizer itr = new StringTokenizer(value.toString(), "-- \t\n\r\f,.:;?![]'\"");
-     
+
         while (itr.hasMoreTokens()) {
-          data.set(itr.nextToken().toLowerCase());
-          context.write(data, one);
+            data.set(itr.nextToken().toLowerCase());
+            context.write(data, one);
         }
-    
     }
 }
